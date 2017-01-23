2d827c140ce15a963f65b9fbe1e105573a507a40
morikuni <taihei.with+git@gmail.com>
2017-01-23 23:20:44 +0900

    Add Parser

---
+++ parser.go
@@ -0,0 +1,42 @@
+package nlftime
+
+import (
+	"errors"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"github.com/olebedev/when/rules/en"
+	"time"
+)
+
+var (
+	// ErrNoTimeFound indicates that no time found in the context.
+	ErrNoTimeFound = errors.New("no time found in the context")
+)
+
+// Parser parse string into time object according to specific base time.
+type Parser interface {
+	Parse(text string, base time.Time) (time.Time, error)
+}
+
+type parser struct {
+	p *when.Parser
+}
+
+func (p parser) Parse(text string, base time.Time) (time.Time, error) {
+	r, err := p.p.Parse(text, base)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if r == nil {
+		return time.Time{}, ErrNoTimeFound
+	}
+	return r.Time, nil
+}
+
+// NewParser creates a new Parser.
+func NewParser() Parser {
+	p := when.New(nil)
+	p.Add(common.All...)
+	p.Add(en.All...)
+	return parser{p}
+}
