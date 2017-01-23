a0645c90796eed80e00860d84d4e76b3d9b2ee5c
morikuni <taihei.with+git@gmail.com>
2017-01-24 08:38:33 +0900

    Add test of Source

---
+++ source_test.go
@@ -0,0 +1,23 @@
+package nlftime
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSource(t *testing.T) {
+	source := NewSource(strings.NewReader(
+		`aaa
+hoge`),
+		5)
+	s, err := source.Generate()
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+	expectedStr :=
+		`aaa
+h`
+	if s != expectedStr {
+		t.Errorf("expected %q but got: %q", expectedStr, s)
+	}
+}
