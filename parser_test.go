29667c1d54e2573395980b759c2469474f7a96c7
morikuni <taihei.with+git@gmail.com>
2017-01-24 00:03:15 +0900

    Add test of Parser

---
+++ parser_test.go
@@ -0,0 +1,20 @@
+package nlftime
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParser(t *testing.T) {
+	parser := NewParser()
+
+	base := time.Date(2009, time.June, 18, 12, 0, 0, 0, time.Local)
+	resultTime, err := parser.Parse("tomorrow 8am", base)
+	if err != nil {
+		t.Errorf("expected nil but got: %v", err)
+	}
+	expectedTime := time.Date(2009, time.June, 19, 8, 0, 0, 0, time.Local)
+	if resultTime != expectedTime {
+		t.Errorf("expected %s but got: %s", expectedTime, resultTime)
+	}
+}
