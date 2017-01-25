541ea6c710c606b73e07a2916c889b19b7fe2bf4
morikuni <taihei.with+git@gmail.com>
2017-01-25 22:53:36 +0900

    Register original time parsers instead of when’s

--- lib/parser.go
+++ lib/parser.go
@@ -2,10 +2,13 @@ package lib
 
 import (
 	"errors"
+	"time"
+
+	"github.com/morikuni/nlftime/lib/whenrule"
 	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules"
 	"github.com/olebedev/when/rules/common"
 	"github.com/olebedev/when/rules/en"
-	"time"
 )
 
 var (
@@ -37,6 +40,12 @@ func (p parser) Parse(text string, base time.Time) (time.Time, error) {
 func NewParser() Parser {
 	p := when.New(nil)
 	p.Add(common.All...)
-	p.Add(en.All...)
+	p.Add(en.Weekday(rules.Override))
+	p.Add(en.CasualDate(rules.Override))
+	p.Add(en.CasualTime(rules.Override))
+	p.Add(en.Deadline(rules.Override))
+	p.Add(whenrule.YMD(rules.Override))
+	p.Add(whenrule.Time(rules.Override))
+	p.Add(whenrule.Hour(rules.Override))
 	return parser{p}
 }
