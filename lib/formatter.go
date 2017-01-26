ce1b866a7770dfa5add248ec521731944946bb73
morikuni <taihei.with+git@gmail.com>
2017-01-27 00:21:00 +0900

    Add unix time formatter

--- lib/formatter.go
+++ lib/formatter.go
@@ -1,8 +1,10 @@
 package lib
 
 import (
-	"github.com/jehiah/go-strftime"
+	"strconv"
 	"time"
+
+	"github.com/jehiah/go-strftime"
 )
 
 // Formatter formats time into a specific format.
@@ -10,13 +12,25 @@ type Formatter interface {
 	Format(string, time.Time) string
 }
 
-type formatter struct{}
+type strftimeFormatter struct{}
 
-func (f formatter) Format(format string, t time.Time) string {
+func (f strftimeFormatter) Format(format string, t time.Time) string {
 	return strftime.Format(format, t)
 }
 
-// NewFormatter creates a new Formatter.
-func NewFormatter() Formatter {
-	return formatter{}
+type unixFormatter struct{}
+
+func (f unixFormatter) Format(_ string, t time.Time) string {
+	return strconv.FormatInt(t.Unix(), 10)
+}
+
+// NewStrftimeFormatter creates a new Formatter for strftime format.
+func NewStrftimeFormatter() Formatter {
+	return strftimeFormatter{}
+}
+
+// NewUnixFormatter creates a new Formatter for unix time.
+// The format arguments are ignored.
+func NewUnixFormatter() Formatter {
+	return unixFormatter{}
 }
