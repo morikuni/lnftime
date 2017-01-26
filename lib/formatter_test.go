c6c6dbf1587bf1723c1a931efd4a68398d0ebb5e
morikuni <taihei.with+git@gmail.com>
2017-01-27 00:24:59 +0900

    Fix test

--- lib/formatter_test.go
+++ lib/formatter_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestFormatter(t *testing.T) {
-	formatter := NewFormatter()
+	formatter := NewStrftimeFormatter("%Y-%m-%d")
 
 	base := time.Date(1992, time.June, 18, 12, 0, 0, 0, time.Local)
-	s := formatter.Format("%Y-%m-%d", base)
+	s := formatter.Format(base)
 	expectedStr := "1992-06-18"
 	if s != expectedStr {
 		t.Errorf("expected %s but got: %s", expectedStr, s)
