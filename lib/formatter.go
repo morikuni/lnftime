63ea778156f76783420dcaad1237417e63e122d7
morikuni <taihei.with+git@gmail.com>
2017-01-27 00:24:55 +0900

    Update Formatter interface

--- lib/formatter.go
+++ lib/formatter.go
@@ -9,28 +9,29 @@ import (
 
 // Formatter formats time into a specific format.
 type Formatter interface {
-	Format(string, time.Time) string
+	Format(time.Time) string
 }
 
-type strftimeFormatter struct{}
+type strftimeFormatter struct {
+	format string
+}
 
-func (f strftimeFormatter) Format(format string, t time.Time) string {
-	return strftime.Format(format, t)
+func (f strftimeFormatter) Format(t time.Time) string {
+	return strftime.Format(f.format, t)
 }
 
 type unixFormatter struct{}
 
-func (f unixFormatter) Format(_ string, t time.Time) string {
+func (f unixFormatter) Format(t time.Time) string {
 	return strconv.FormatInt(t.Unix(), 10)
 }
 
 // NewStrftimeFormatter creates a new Formatter for strftime format.
-func NewStrftimeFormatter() Formatter {
-	return strftimeFormatter{}
+func NewStrftimeFormatter(format string) Formatter {
+	return strftimeFormatter{format}
 }
 
 // NewUnixFormatter creates a new Formatter for unix time.
-// The format arguments are ignored.
 func NewUnixFormatter() Formatter {
 	return unixFormatter{}
 }
